0bbc75305d28384755f0b650a37ff1ae74756f41
Jan Jonas <mail@janjonas.net>
2015-10-28 22:10:20 +0100

    Changed (developer) website URL from http://dnndevtools.develop to http://localhost/DnnDevTools

--- DnnDevTools/Src/DnnDevTools/Api/MailController.cs
+++ DnnDevTools/Src/DnnDevTools/Api/MailController.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using System.Web.Http;
 using DotNetNuke.Web.Api;
 
-namespace weweave.DnnDevTools
+namespace weweave.DnnDevTools.Api
 {
     public class MailController : DnnApiController
     {
--- DnnDevTools/Src/DnnDevTools/HttpModule.cs
+++ DnnDevTools/Src/DnnDevTools/HttpModule.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using DotNetNuke.Framework;
+using DotNetNuke.Web.Client;
+using DotNetNuke.Web.Client.ClientResourceManagement;
+using DotNetNuke.Web.Client.Providers;
 
 namespace weweave.DnnDevTools
 {
@@ -33,10 +37,13 @@ namespace weweave.DnnDevTools
         {
             var page = (Page)sender;
 
+            ServicesFramework.Instance.RequestAjaxScriptSupport();
+
             var bodyControl = page?.FindControl("Body") as HtmlContainerControl;
             if (bodyControl == null) return;
 
-            var toolbarHtml = @"<script src=""/desktopmodules/DnnDevTools/Scripts/jquery.signalR-2.2.0.js""></script><script src=""signalr/hubs""></script>";
+            ClientResourceManager.RegisterScript(page, "~/desktopmodules/DnnDevTools/Scripts/jquery.signalR-2.2.0.js", FileOrder.Js.DefaultPriority, DnnFormBottomProvider.DefaultName);
+            var toolbarHtml = $@"<script src=""{HostingEnvironment.ApplicationVirtualPath}/signalr/hubs""></script>";
             toolbarHtml += System.IO.File.ReadAllText(
                 HttpContext.Current.Server.MapPath("~/DesktopModules/DnnDevTools/Toolbar.html")
             );

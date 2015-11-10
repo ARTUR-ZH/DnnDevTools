f6eb543a7474266bbaf89949b46d0d146a0f34fa
Jan Jonas <mail@janjonas.net>
2015-11-10 22:28:54 +0100

    HttpModule now fixes/adds smtp setting section

--- DnnDevTools/Src/DnnDevTools/HttpModule.cs
+++ DnnDevTools/Src/DnnDevTools/HttpModule.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Configuration;
+using System.Net.Configuration;
+using System.Net.Mail;
+using System.Threading;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Hosting;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
@@ -14,11 +19,60 @@ namespace weweave.DnnDevTools
     {
         public void Init(HttpApplication context)
         {
+            context.BeginRequest += OnBeginRequest;
             context.PreRequestHandlerExecute += OnPreRequestHandlerExecute;
         }
 
         public void Dispose()
         {
+
+        }
+
+        private static void OnBeginRequest(object sender, EventArgs e)
+        {
+            var request = HttpContext.Current.Request;
+
+            if (
+                !"get".Equals(request.HttpMethod, StringComparison.OrdinalIgnoreCase) || 
+                (request.AppRelativeCurrentExecutionFilePath != null && (
+                    request.AppRelativeCurrentExecutionFilePath.StartsWith("~/signalr/", StringComparison.OrdinalIgnoreCase) ||
+                    request.AppRelativeCurrentExecutionFilePath.StartsWith("~/WebResource.axd", StringComparison.OrdinalIgnoreCase))
+                )
+            ) return;
+
+            var configuration = WebConfigurationManager.OpenWebConfiguration("~");
+
+            var section = configuration.GetSection("system.net/mailSettings/smtp") as SmtpSection;
+
+            var saveConfig = false;
+            if (section == null)
+            {
+                var smtpSection = new SmtpSection();
+                smtpSection.SpecifiedPickupDirectory.PickupDirectoryLocation = MailPickupFileWatcher.Path;
+                smtpSection.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                configuration.Sections.Add("system.net/mailSettings/smtp", smtpSection);
+                saveConfig = true;
+            }
+            else
+            {
+                if (section.DeliveryMethod != SmtpDeliveryMethod.SpecifiedPickupDirectory)
+                {
+                    section.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    saveConfig = true;
+                }
+                if (section.SpecifiedPickupDirectory.PickupDirectoryLocation != MailPickupFileWatcher.Path)
+                {
+                    section.SpecifiedPickupDirectory.PickupDirectoryLocation = MailPickupFileWatcher.Path;
+                    saveConfig = true;
+                }
+            }
+            if (saveConfig)
+            {
+                new Thread(() =>
+                {
+                    configuration.Save();
+                }).Start();
+            }
 
         }
 
@@ -30,7 +84,6 @@ namespace weweave.DnnDevTools
             if (page == null) return;
 
             page.Init += OnPageInit;
-
         }
 
         private static void OnPageInit(object sender, EventArgs e)

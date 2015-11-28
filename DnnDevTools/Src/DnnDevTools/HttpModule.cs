e25835b1644348a2fe807b09b487656cf1fe95ba
Jan Jonas <mail@janjonas.net>
2015-11-28 16:24:46 +0100

    Added overlay (URL) to toolbar's JavaScript config

--- DnnDevTools/Src/DnnDevTools/HttpModule.cs
+++ DnnDevTools/Src/DnnDevTools/HttpModule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Configuration;
@@ -17,32 +18,31 @@ using DotNetNuke.Framework;
 using DotNetNuke.Web.Client;
 using DotNetNuke.Web.Client.ClientResourceManagement;
 using DotNetNuke.Web.Client.Providers;
+using Newtonsoft.Json;
 using weweave.DnnDevTools.SignalR;
 
 namespace weweave.DnnDevTools
 {
-    public class LocalizeResourceFilter : MemoryStream
-    {
-        private readonly Stream _outputStream;
-        public LocalizeResourceFilter(Stream outputStream)
-        {
-            _outputStream = outputStream;
-        }
-
-        public override void Write(byte[] buffer, int offset, int count)
-        {
-            var contentInBuffer = Encoding.UTF8.GetString(buffer);
-
-            var rsxr = new ResXResourceReader(HttpContext.Current.Server.MapPath("~/DesktopModules/DnnDevTools/App_LocalResources/Overlay.aspx.resx"));
-
-            contentInBuffer = rsxr.Cast<DictionaryEntry>().Aggregate(contentInBuffer, (current, d) => current.Replace($"[res:{d.Key}]", d.Value.ToString()));
-
-            _outputStream.Write(Encoding.UTF8.GetBytes(contentInBuffer), offset, Encoding.UTF8.GetByteCount(contentInBuffer));
-        }
-    }
-
     public class HttpModule : IHttpModule
     {
+        private class LocalizeResourceFilter : MemoryStream
+        {
+            private readonly Stream _outputStream;
+            public LocalizeResourceFilter(Stream outputStream)
+            {
+                _outputStream = outputStream;
+            }
+
+            public override void Write(byte[] buffer, int offset, int count)
+            {
+                var contentInBuffer = Encoding.UTF8.GetString(buffer);
+                var rsxr = new ResXResourceReader(HttpContext.Current.Server.MapPath("~/DesktopModules/DnnDevTools/App_LocalResources/Overlay.aspx.resx"));
+                contentInBuffer = rsxr.Cast<DictionaryEntry>().Aggregate(contentInBuffer, (current, d) => current.Replace($"[res:{d.Key}]", d.Value.ToString()));
+                _outputStream.Write(Encoding.UTF8.GetBytes(contentInBuffer), offset, Encoding.UTF8.GetByteCount(contentInBuffer));
+            }
+        }
+
+
         public void Init(HttpApplication context)
         {
             context.BeginRequest += OnBeginRequest;
@@ -147,10 +147,16 @@ namespace weweave.DnnDevTools
             var rsxr = new ResXResourceReader(HttpContext.Current.Server.MapPath("~/DesktopModules/DnnDevTools/App_LocalResources/Toolbar.html.resx"));
             toolbarHtml = rsxr.Cast<DictionaryEntry>().Aggregate(toolbarHtml, (current, d) => current.Replace($"[res:{d.Key}]", d.Value.ToString()));
 
+            // Build JavaScript config
+            var javaScriptConfig = new Dictionary<string, object>
+            {
+                ["enableMailCatch"] = ServiceLocatorFactory.Instance.ConfigService.GetEnableMailCatch(),
+                ["overlay"] = $"{HostingEnvironment.ApplicationVirtualPath}/DesktopModules/DnnDevTools/Overlay.aspx"
+            };
+
             // Inject HTML into end of body
-            var enableMailCatch = ServiceLocatorFactory.Instance.ConfigService.GetEnableMailCatch();
             var html = $@"<script src=""{HostingEnvironment.ApplicationVirtualPath}/signalr/hubs""></script>";
-            html += @"<script type=""text/javascript"">window.dnnMailDev={enableMailCatch: " + enableMailCatch.ToString().ToLowerInvariant() + "}</script>";
+            html += $@"<script type=""text/javascript"">window.dnnMailDev={JsonConvert.SerializeObject(javaScriptConfig)}</script>";
             html += toolbarHtml;
             var scriptControl = new LiteralControl { Text = html };
             bodyControl.Controls.Add(scriptControl);

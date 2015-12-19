6d9087982f8cdc12536152d52454349027aa9f46
Jan Jonas <mail@janjonas.net>
2015-12-19 21:49:29 +0100

    Log4NetUtil.ParseLevel now works wothout configured log4net

---
+++ DnnDevTools.Test/Src/Util/Log4NetUtil/ParseLevelTest.cs
@@ -0,0 +1,29 @@
+﻿using log4net.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace weweave.DnnDevTools.Test.Util.Log4NetUtil
+{
+
+    [TestClass]
+    public class ParseLevelTest
+    {
+
+        [TestMethod]
+        public void ParseLevelInfo()
+        {
+            Assert.AreEqual(Level.Info, DnnDevTools.Util.Log4NetUtil.ParseLevel("info"));
+        }
+
+        [TestMethod]
+        public void ParseLevelInfoCaseInsensitive()
+        {
+            Assert.AreEqual(Level.Info, DnnDevTools.Util.Log4NetUtil.ParseLevel("iNfO"));
+        }
+
+        [TestMethod]
+        public void ParseUnkownLevel()
+        {
+            Assert.IsNull(DnnDevTools.Util.Log4NetUtil.ParseLevel("UnkownLevel"));
+        }
+    }
+}
--- DnnDevTools/Src/DnnDevTools/Util/Log4NetUtil.cs
+++ DnnDevTools/Src/DnnDevTools/Util/Log4NetUtil.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using log4net.Core;
 
 namespace weweave.DnnDevTools.Util
@@ -8,15 +7,41 @@ namespace weweave.DnnDevTools.Util
     {
         public static Level ParseLevel(string level)
         {
-            var loggerRepository = LoggerManager.GetAllRepositories().First();
-
-            if (loggerRepository == null)
-            {
-                throw new Exception("No logging repositories defined");
-            }
-
-            var stronglyTypedLevel = loggerRepository.LevelMap[level];
-            return stronglyTypedLevel;
+            if ("info".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Info;
+            if ("alert".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Alert;
+            if ("all".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.All;
+            if ("critical".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Critical;
+            if ("debug".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Debug;
+            if ("emergency".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Emergency;
+            if ("error".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Error;
+            if ("fatal".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Fatal;
+            if ("fine".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Fine;
+            if ("finer".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Finer;
+            if ("finest".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Finest;
+            if ("notice".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Notice;
+            if ("all".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.All;
+            if ("off".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Off;
+            if ("severe".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Severe;
+            if ("trace".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Trace;
+            if ("verbose".Equals(level, StringComparison.OrdinalIgnoreCase))
+                return Level.Verbose;
+            return "warn".Equals(level, StringComparison.OrdinalIgnoreCase) ? Level.Warn : null;
         }
     }
 }

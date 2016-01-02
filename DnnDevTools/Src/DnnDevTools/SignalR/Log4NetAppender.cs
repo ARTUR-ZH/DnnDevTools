35c6f1b06a7bcfc79ac548bf3df9717701b28597
Jan Jonas <mail@janjonas.net>
2016-01-02 12:36:28 +0100

    Increased Log4Net queue size to 5.000

--- DnnDevTools/Src/DnnDevTools/SignalR/Log4NetAppender.cs
+++ DnnDevTools/Src/DnnDevTools/SignalR/Log4NetAppender.cs
@@ -9,7 +9,7 @@ namespace weweave.DnnDevTools.SignalR
 {
     internal class Log4NetAppender : AppenderSkeleton
     {
-        private const int QueueSize = 1000;
+        private const int QueueSize = 5000;
 
         internal readonly static ConcurrentQueue<LogMessage> LogMessageQueue = new ConcurrentQueue<LogMessage>();
 

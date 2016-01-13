bc96ab9529d0d2a6fc327be2d0c19b7ec4ef297e
Jan Jonas <mail@janjonas.net>
2016-01-13 22:27:17 +0100

    #7: Added detail API for DnnEventController and LogController

--- DnnDevTools/Src/DnnDevTools/Api/Controller/DnnEventController.cs
+++ DnnDevTools/Src/DnnDevTools/Api/Controller/DnnEventController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 using DotNetNuke.Web.Api;
 using weweave.DnnDevTools.Dto;
@@ -23,5 +26,13 @@ namespace weweave.DnnDevTools.Api.Controller
             return ServiceLocator.DnnEventService.GetList(start, skip, take, search);
         }
 
+        [HttpGet]
+        public HttpResponseMessage Detail(string id)
+        {
+            var dnnEvent = ServiceLocator.DnnEventService.GetList(null, null, null, null).FirstOrDefault(e => e.Id == id);
+            return dnnEvent == null ?
+                Request.CreateResponse(HttpStatusCode.NotFound) :
+                Request.CreateResponse(HttpStatusCode.OK, dnnEvent);
+        }
     }
 }
--- DnnDevTools/Src/DnnDevTools/Api/Controller/LogController.cs
+++ DnnDevTools/Src/DnnDevTools/Api/Controller/LogController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 using DotNetNuke.Web.Api;
 using weweave.DnnDevTools.Dto;
@@ -21,5 +24,15 @@ namespace weweave.DnnDevTools.Api.Controller
         {
             return ServiceLocator.LogService.GetList(start, skip, take, search);
         }
+
+        [HttpGet]
+        public HttpResponseMessage Detail(string id)
+        {
+            var logMessage = ServiceLocator.LogService.GetList(null, null, null, null).FirstOrDefault(e => e.Id == id);
+            return logMessage == null ?
+                Request.CreateResponse(HttpStatusCode.NotFound) :
+                Request.CreateResponse(HttpStatusCode.OK, logMessage);
+        }
+
     }
 }

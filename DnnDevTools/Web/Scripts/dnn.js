ce980c52ba028ac563b2945f3ab6e9f8138448a1
Matthias Benfer <matthias_benfer@jack-wolfskin.com>
2016-01-17 18:13:01 +0100

    #13: replaced Promise functionality with callbacks to provide ie support;

--- DnnDevTools/Web/Scripts/dnn.js
+++ DnnDevTools/Web/Scripts/dnn.js
@@ -1,28 +1,28 @@
 ﻿(function () {
     dnnDevTools = {
-        ajax: function (method, url) {
-            return new Promise(function (resolve, reject) {
-                var req = new XMLHttpRequest();
+        ajax: function (method, url, success, error) {
+            var req = new XMLHttpRequest();
 
-                req.open(method, url);
+            req.open(method, url);
 
-                req.setRequestHeader('requestVerificationToken', $.ServicesFramework().getAntiForgeryValue());
+            req.setRequestHeader('requestVerificationToken', $.ServicesFramework().getAntiForgeryValue());
 
-                req.onload = function () {
-                    if (req.status == 200) {
-                        resolve(req.response);
-                    }
-                    else {
-                        reject(Error(req.statusText));
-                    }
-                };
+            req.onload = function () {
+                var response = (req.responseText && req.responseText != '') ? JSON.parse(req.responseText) : '';
 
-                req.onerror = function () {
-                    reject(Error("Please check your internet connection."));
-                };
+                if (req.status == 200) {
+                    success(response);
+                }
+                else {
+                    error(Error(req.statusText));
+                }
+            };
 
-                req.send();
-            });
+            req.onerror = function () {
+                error(Error("Please check your internet connection."));
+            };
+
+            req.send();
         }
     }
 }());
